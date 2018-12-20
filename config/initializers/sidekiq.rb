c4ce9be6a085e01380a9e4c3b1d276b8bbc8c913
Carlos Ramseyer <cmramseyer@gmail.com>
2018-12-19 21:37:46 -0300

    added redis pass to sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}" }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}" }
+  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'] }
 end
 
 Sidekiq.default_worker_options = { retry: 0 }
