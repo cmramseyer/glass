882cc4ff5f21eabffd69005997b8b25d68e75862
Carlos Ramseyer <cmramseyer@gmail.com>
2019-01-04 14:06:21 -0300

    solved sidebar active

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -36,6 +36,22 @@ function turn_on_sidebar_menu(){
 
     $('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
   });
+
+  // check active menu
+  $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+
+  $SIDEBAR_MENU.find('a').filter(function () {
+      return this.href == CURRENT_URL;
+  }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
+      setContentHeight();
+  }).parent().addClass('active');
+
+  // recompute content when resizing
+  $(window).smartresize(function(){  
+      setContentHeight();
+  });
+
+  setContentHeight();
 }
 
 
