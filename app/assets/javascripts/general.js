8b4a663b8cfb01fad091f33e64a676baf6af2e73
Carlos Ramseyer <cmramseyer@gmail.com>
2019-01-04 13:41:46 -0300

    solved sidebar bug

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -8,12 +8,42 @@ function turn_on_checkbox_style(){
   }
 }
 
+function turn_on_sidebar_menu(){
+  var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
+    $BODY = $('body'),
+    $MENU_TOGGLE = $('#menu_toggle'),
+    $SIDEBAR_MENU = $('#sidebar-menu'),
+    $SIDEBAR_FOOTER = $('.sidebar-footer'),
+    $LEFT_COL = $('.left_col'),
+    $RIGHT_COL = $('.right_col'),
+    $NAV_MENU = $('.nav_menu'),
+    $FOOTER = $('footer');
+
+
+  // toggle small or large menu
+  $MENU_TOGGLE.on('click', function() {
+    if ($BODY.hasClass('nav-md')) {
+        $SIDEBAR_MENU.find('li.active ul').hide();
+        $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+    } else {
+        $SIDEBAR_MENU.find('li.active-sm ul').show();
+        $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+    }
+
+    $BODY.toggleClass('nav-md nav-sm');
+
+    setContentHeight();
+
+    $('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
+  });
+}
+
+
+
 $( document ).on('turbolinks:load', function() {
   $('[data-toggle="tooltip"]').tooltip(); 
 
-
-
-  
+  turn_on_sidebar_menu();
   turn_on_checkbox_style();
   
 });
