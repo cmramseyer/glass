6dd62d9ae9c90f32991e0aef26345055d32ac9a1
Carlos Ramseyer <cmramseyer@gmail.com>
2021-07-03 15:24:19 -0300

    refactored sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,13 @@
-if Rails.env.development?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", db: 5 }
-  end
+common_redis_config = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", db: 5 }
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", db: 5 }
-  end
-else
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
-  end
+common_redis_config[:password] = ENV['REDIS_PASSWORD'] if Rails.env.production?
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = common_redis_config
+end
 
+Sidekiq.configure_client do |config|
+  config.redis = common_redis_config
 end
 
 Sidekiq.default_worker_options = { retry: 0 }
