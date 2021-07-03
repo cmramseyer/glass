2ede71528f44a1b4b0aff2617dc6825365e6a9c0
Carlos Ramseyer <cmramseyer@gmail.com>
2021-07-02 22:15:17 -0300

    added .env.example file, adjust local dev config for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,20 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
-end
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", db: 5 }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", db: 5 }
+  end
+else
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['SIDEKIQ_REDIS_URL'] , namespace: "activejob_#{Rails.env}", password: ENV['REDIS_PASSWORD'], db: 5 }
+  end
+
 end
 
 Sidekiq.default_worker_options = { retry: 0 }
